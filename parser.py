ae92baf330551b174756091f25e36403e08afe28
donhilion <donhilion@googlemail.com>
2013-01-14 16:39:37 +0100

    Added eval methods.

--- parser.py
+++ parser.py
@@ -19,6 +19,9 @@ class Const(Exp):
     def __init__(self, value):
         self.value = value
 
+    def eval(self, env):
+        return self.value
+
     def __str__(self):
         return "Constant(%s)" % self.value
 
@@ -26,6 +29,14 @@ class Variable(Exp):
 
     def __init__(self, name):
         self.name = name
+
+    def get_name(self):
+        return self.name
+
+    def eval(self, env):
+        if self.name not in env:
+            return UNDEFINED
+        return env[self.name]
 
     def __str__(self):
         return "Variable(%s)" % self.name
@@ -36,6 +47,12 @@ class Assignment(Exp):
         self.variable = variable
         self.exp = exp
 
+    def eval(self, env):
+        if self.variable in env:
+            env[self.variable] = self.exp.eval(env)
+        else:
+            raise Exception('Variable %s not defined.' % self.variable.get_name())
+
     def __str__(self):
         return "Assignment(%s, %s)" % (self.variable, self.exp)
 
@@ -44,6 +61,11 @@ class Declaration(Exp):
     def __init__(self, variable, exp):
         self.variable = variable
         self.exp = exp
+
+    def eval(self, env):
+        if self.variable in env:
+            raise Exception('Variable %s already defined.' % self.variable.get_name())
+        env[self.variable] = self.exp.eval(env)
 
     def __str__(self):
         return "Declaration(%s, %s)" % (self.variable, self.exp)
@@ -55,6 +77,12 @@ class IfThenElse(Exp):
         self.then = then
         self.els = els
 
+    def eval(self, env):
+        if self.cond.eval(env):
+            self.then.eval(env)
+        elif self.els is not None:
+            self.els.eval(env)
+
     def __str__(self):
         return "If(%s, %s, %s)" % (self.cond, self.then, self.els)
 
@@ -64,6 +92,10 @@ class While(Exp):
         self.cond = cond
         self.do = do
 
+    def eval(self, env):
+        while self.cond.eval(env):
+            self.do.eval(env)
+
     def __str__(self):
         return "While(%s, %s)" % (self.cond, self.do)
 
@@ -71,6 +103,10 @@ class CmdList(Exp):
 
     def __init__(self, lst):
         self.lst = lst
+
+    def eval(self, env):
+        for cmd in self.lst:
+            cmd.eval(env)
 
     def __str__(self):
         string = ""
@@ -83,6 +119,9 @@ class Print(Exp):
     def __init__(self, exp):
         self.exp = exp
 
+    def eval(self, env):
+        print(self.exp.eval(env))
+
     def __str__(self):
         return "Print(%s)" % self.exp
 
@@ -91,6 +130,9 @@ class Add(Exp):
     def __init__(self, left, right):
         self.left = left
         self.right = right
+
+    def eval(self, env):
+        return self.left.eval(env) + self.right.eval(env)
 
     def __str__(self):
         return "Add(%s, %s)" % (self.left, self.right)
@@ -101,6 +143,9 @@ class Sub(Exp):
         self.left = left
         self.right = right
 
+    def eval(self, env):
+        return self.left.eval(env) - self.right.eval(env)
+
     def __str__(self):
         return "Sub(%s, %s)" % (self.left, self.right)
 
@@ -109,6 +154,9 @@ class Mul(Exp):
     def __init__(self, left, right):
         self.left = left
         self.right = right
+
+    def eval(self, env):
+        return self.left.eval(env) * self.right.eval(env)
 
     def __str__(self):
         return "Mul(%s, %s)" % (self.left, self.right)
@@ -119,6 +167,9 @@ class Div(Exp):
         self.left = left
         self.right = right
 
+    def eval(self, env):
+        return self.left.eval(env) / self.right.eval(env)
+
     def __str__(self):
         return "Div(%s, %s)" % (self.left, self.right)
 
@@ -127,6 +178,9 @@ class Eq(Exp):
     def __init__(self, left, right):
         self.left = left
         self.right = right
+
+    def eval(self, env):
+        return self.left.eval(env) == self.right.eval(env)
 
     def __str__(self):
         return "Eq(%s, %s)" % (self.left, self.right)
@@ -137,6 +191,9 @@ class Lt(Exp):
         self.left = left
         self.right = right
 
+    def eval(self, env):
+        return self.left.eval(env) < self.right.eval(env)
+
     def __str__(self):
         return "Lt(%s, %s)" % (self.left, self.right)
 
@@ -145,6 +202,9 @@ class Gt(Exp):
     def __init__(self, left, right):
         self.left = left
         self.right = right
+
+    def eval(self, env):
+        return self.left.eval(env) > self.right.eval(env)
 
     def __str__(self):
         return "Gt(%s, %s)" % (self.left, self.right)
@@ -155,6 +215,7 @@ def tokenize(string):
     Generates a list of tokens from the given string.
     """
     specs = [
+        ('Space',		(r'[ \t\r\n]+',)),
         ('Op',          (r'[\-+/*=<>]',)),
         ('Assign',      (r':=',)),
         ('Ident', 		(r'[A-Za-z][A-Za-z_0-9]*',)),
@@ -162,12 +223,12 @@ def tokenize(string):
         ('Semicolon',	(';',)),
         ('Lb',          ('{',)),
         ('Rb',          ('}',)),
-        ('Lp',          ('(',)),
-        ('Rp',          (')',)),
+        ('Lp',          ('\(',)),
+        ('Rp',          ('\)',)),
         ]
     useless = ['Space']
     t = make_tokenizer(specs)
-    return [x for x in t(str) if x.type not in useless]
+    return [x for x in t(string) if x.type not in useless]
 
 def parse(seq):
     """
@@ -184,8 +245,10 @@ def parse(seq):
     op = lambda s: a(Token('Op', s)) >> tokval # return the value if token is Op
     op_ = lambda s: skip(op(s)) # checks if token is Op and ignores it
 
+    ident = lambda s: a(Token('Ident', s)) >> tokval # return the value if token is Op
+    ident_ = lambda s: skip(ident(s)) # checks if token is Op and ignores it
+
     lst = lambda x: [x[0],] + x[1]
-    tup = lambda x: (x[0], x[1])
 
     makeop = lambda s, f: op(s) >> const(f)
 
@@ -203,24 +266,30 @@ def parse(seq):
     comp_op = lt | gt | eq
 
     assign = with_forward_decls(lambda: toktype('Ident') + skip(toktype('Assign')) + exp >> unarg(Assignment))
-    ifexp = with_forward_decls(lambda: skip(Token('Ident', "if")) + cond + cmd + \
-                                       maybe(skip(Token("Ident", "else")) + cmd) >> unarg(IfThenElse))
-    whileexp = with_forward_decls(lambda: skip(Token("Ident", "while")) + cond + cmd >> unarg(While))
-    printexp = with_forward_decls(lambda: skip(Token("Ident", "print")) + exp >> unarg(Print))
-    vardef = with_forward_decls(lambda: skip(Token("Ident", "var")) + toktype("Ident") + op_("=") + exp \
+    ifexp = with_forward_decls(lambda: ident_('if') + cond + cmd + \
+                                       maybe(ident_('else') + cmd) >> unarg(IfThenElse))
+    whileexp = with_forward_decls(lambda: ident_('while') + cond + cmd >> unarg(While))
+    printexp = with_forward_decls(lambda: ident_('print') + exp >> Print)
+    vardef = with_forward_decls(lambda: ident_('var') + toktype('Ident') + op_('=') + exp \
                                         >> unarg(Declaration))
 
     cmd = with_forward_decls(lambda: assign | ifexp | whileexp | printexp | vardef | \
-                                        skip(toktype('Lb')) + cmd_list + skip(toktype("Rb")))
-    cmd_list = (cmd + many(skip(toktype("Semicolon")) + cmd) >> lst) >> unarg(CmdList)
+                                        skip(toktype('Lb')) + cmd_list + skip(toktype('Rb')))
+    cmd_list = (cmd + many(skip(toktype('Semicolon')) + cmd) >> lst) >> CmdList
 
-    variable = toktype("Ident") >> unarg(Variable)
-    constexp = toktype("Number") >> unarg(Const)
+    variable = toktype('Ident') >> Variable
+    constexp = toktype('Number') >> Const
     factor = with_forward_decls(lambda: variable | constexp | \
-                                        skip(toktype("Lp")) + exp + skip(toktype("Rp")))
-    summand = factor + maybe(point_op + factor) >> unarg(eval_expr)
-    exp = summand + maybe(line_op + summand) >> unarg(eval_expr)
+                                        skip(toktype('Lp')) + exp + skip(toktype('Rp')))
+    summand = factor + many(point_op + factor) >> unarg(eval_expr)
+    exp = with_forward_decls(lambda: summand + many(line_op + summand) >> unarg(eval_expr) | \
+                                        cond)
 
     cond = exp + comp_op + exp >> eval_cond
 
     return cmd.parse(seq)
+
+parsed = parse(tokenize('{ var x = 5; print x }'))
+print(str(parsed))
+parsed.eval({})
+
